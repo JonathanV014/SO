6fcd37945489ddd691612b8c5d810a377322a865
Jonathan Enrrique Vizcaino Macias <jevizcaino@unimagdalena.edu.co>
2024-10-18 17:33:56 -0500

    Modificacion para hacer que todo sea dinamico, ahora puedes pedir los tamanos de las filas y columnas por teclado

--- Procesos/Procesos con Memoria Compartida/Matriz/matrizSM.c
+++ Procesos/Procesos con Memoria Compartida/Matriz/matrizSM.c
@@ -6,28 +6,32 @@
 #include <sys/stat.h>
 
 int main(int argc, char *argv[]){
-    int filas = 6;
-    int columnas = 6;
-    int vecIdColumnas[filas];
+    int filas; printf("Filas: "); scanf("%d", &filas);
+    int columnas; printf("Columnas: "); scanf("%d", &columnas);
     
-    //ESPACIO DE MEMORIA COMPARTIDA PARA QUE CADA HIJO ESCRIBA SU RESULTADO DE LA SUMA
+    //VECTOR EN MEMORIA COMPARTIDA PARA GUARDAR LOS ID DE LOS ESPACIOS PARA CADA COLUMNA
+    int *vecIdColumnas = NULL;
+    int idVecIdColumnas = shmget(IPC_PRIVATE, filas * sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR);
+    vecIdColumnas = shmat(idVecIdColumnas, 0, 0);
+
+    //SE RESERVA ESPACIO PARA LA MATRIZ, EN CONCRETO ESPACIO PARA CADA FILA
     int **matriz;
     int shmTamMatrizF = (filas) * sizeof(int *); 
     int shmTamMatrizC = (columnas) * sizeof(int);
     int idMatriz = shmget(IPC_PRIVATE, shmTamMatrizF, IPC_CREAT | S_IRUSR | S_IWUSR); //CLAVE PA CONECTARSE
     matriz = (int **) shmat(idMatriz, 0, 0);
 
+    //RESEVAR ESPACIO PARA CADA COLUMNA GUARDANDO EL ID DEL ESPACIO DE MEMORIA DE CADA COLUMNA
     for (int i = 0; i < filas; i++){
         vecIdColumnas[i] = shmget(IPC_PRIVATE, shmTamMatrizC, IPC_CREAT | S_IRUSR | S_IWUSR);
         matriz[i] = (int *) shmat(vecIdColumnas[i], 0, 0);
     }
-        
-
-    
 
     if (fork()){
+        //ESPERAR A QUE EL HIJO LLENE LA MATRIZ
         wait(NULL);
 
+        //MOSTRAR EN PANTALA LOS DATOS DE LA MATRIZ
         for (int i = 0; i < filas; i++){
             for (int j = 0; j < columnas; j++){
                 printf("[%d] ", matriz[i][j]);
@@ -35,25 +39,36 @@ int main(int argc, char *argv[]){
             printf("\n");
         }
 
+        //SEPARAR AL PADRE DEL ESPACIO DE MEMORIA PARA CADA COLUMNA DENTRO DE LA MATRIZ Y ELIMINANDO LA MEMORIA
         for (int i = 0; i < filas; i++){
             shmdt(matriz[i]);
             shmctl(vecIdColumnas[i], IPC_RMID, 0);
         }
+
+        //SEPARAR AL PADRE DEL ESPACIO DE MEMORIA PARA LA MATRIZ Y ELIMINAR LA MEMORIA PARA LA MATRIZ
         shmdt(matriz);
         shmctl(idMatriz, IPC_RMID, 0);
 
-        
+        //SEPARAR AL PADRE DEL ESPACIO DE MEMPORIA PARA EL VECTOR DE ID DE MEMORIA DE LA COLUMNA Y ELIMINAR LA MEMORIA
+        shmdt(vecIdColumnas);
+        shmctl(idVecIdColumnas, IPC_RMID, 0);
     }else{
+
+        //LLENAR DE DATOS LA MATRIZ CON EL HIJO
         for (int i = 0; i < filas; i++){
             for (int j = 0; j < columnas; j++){
                 matriz[i][j] = 1;
             }
         }
 
+        //SEPARAR AL HIJO DE LOS ESPACIOS DE MEMORIA PARA CADA COLUMNA
         for (int i = 0; i < filas; i++){
             shmdt(matriz[i]);
         }
+
+        //SEPARAR AL HIJO DEL ESPACIO DE MEMORIA DE LA MATRIZ Y DEL VECTOR DE ID DE MEMORIA PARA CADA COLUMNA
         shmdt(matriz);
+        shmdt(vecIdColumnas);
     }
 
     
