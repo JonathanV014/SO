69ffe0e55adb622fcb9a9edcfa899960d3d19e63
Jonathan Enrrique Vizcaino Macias <jevizcaino@unimagdalena.edu.co>
2024-11-23 01:05:31 -0500

    Ejercicio completado de relajacion de jacobi

--- Hilos/Relajacion de Jacobi/main.c
+++ Hilos/Relajacion de Jacobi/main.c
@@ -23,10 +23,36 @@ typedef struct data{
     int i;
 }data;
 
+pthread_barrier_t mybarrier1;
+pthread_barrier_t mybarrier2;
+
+int calcularRelajacionJacobi(int i, int j){
+    return((matriz_d_datos[i-1][j] + matriz_d_datos[i+1][j] + matriz_d_datos[i][j-1] + matriz_d_datos[i][j+1])/4);
+}
 
 void *funcionHilo(void *x){
+    //printf("Parte %d Restante %d\n", parte, restante);
     data *datos = (data *)x;
-    printf("%d\n", datos->i);
+    int inicio = datos->i * parte;
+    int fin = ((datos->i) + 1) * parte;
+    if (datos->i == (nHilos-1)){
+        fin += restante;
+    }
+
+    for (int k = 0; k < tiempos; k++){
+        for (int i = (inicio + 1); i < (fin + 1); i++){
+            for (int j = 0; j < nColumnas; j++){
+                if (j != 0 && j != (nColumnas-1)){
+                    matriz_d_resultado[i][j] = calcularRelajacionJacobi(i, j);
+                }
+            }
+        }
+        pthread_barrier_wait(&mybarrier1);
+        pthread_barrier_wait(&mybarrier2);
+    }
+    
+    
+    
 
     pthread_exit(EXIT_SUCCESS);
 }
@@ -47,6 +73,10 @@ int main(int argc, char const *argv[]){
     fscanf(archivo_data, "%d %d", &nFilas, &nColumnas);
     printf("#FILAS: %d | #COLUMNAS: %d\n", nFilas, nColumnas);
 
+    if (nFilas <= 2 && nColumnas <= 2){
+        printf("Matriz no valida para calcilo\n");
+        exit(EXIT_FAILURE);
+    }
     
     int filas_m_d_datos = nFilas;
     int columnas_m_d_datos = nColumnas;
@@ -74,7 +104,7 @@ int main(int argc, char const *argv[]){
     for (int pi = 0; pi < filas_m_d_datos; pi++){
         for (int pj = 0; pj < columnas_m_d_datos; pj++){
             fscanf(archivo_data, "%d", &matriz_d_datos[pi][pj]);
-            matriz_d_resultado[pi][pj] = 0;
+            matriz_d_resultado[pi][pj] = matriz_d_datos[pi][pj];
             printf("[%d]\t", matriz_d_datos[pi][pj]);
         }
         printf("\n");
@@ -87,13 +117,14 @@ int main(int argc, char const *argv[]){
     }
 
     printf("Digite el tiempo: "); scanf("%d", &tiempos);
-    if (nHilos <= 0){
+    if (tiempos <= 0){
         printf("Numero de tiempo no valido, debe ser mayor a 0\n");
         exit(EXIT_FAILURE);
     }
 
-    parte = (nFilas-2)/nHilos;
+    parte = (nFilas-2) / nHilos;
     restante = (nFilas-2) - (parte * nHilos);
+
 
     int tam_vector_d_hilos = nHilos;
     vector_d_hilos = (pthread_t *) malloc(tam_vector_d_hilos * sizeof(pthread_t));
@@ -101,6 +132,9 @@ int main(int argc, char const *argv[]){
         printf("No se pudo reservar espacio para vector_d_hilos\n");
         exit(EXIT_FAILURE);
     }
+
+    pthread_barrier_init(&mybarrier1, NULL, nHilos+1);
+    pthread_barrier_init(&mybarrier2, NULL, nHilos+1);
 
     for (int i = 0; i < nHilos; i++){
         data *datos = NULL;
@@ -113,13 +147,31 @@ int main(int argc, char const *argv[]){
         pthread_create(&vector_d_hilos[i], NULL, funcionHilo, (void *)datos);   
     }
 
-    for (int i = 0; i < nHilos; i++){
-        pthread_join(vector_d_hilos[i], NULL);
+
+    for (int k = 0; k < tiempos; k++){
+        printf("Resultado tiempo %d\n", k+1);
+        pthread_barrier_wait(&mybarrier1);
+
+        for (int pi = 0; pi < nFilas; pi++){
+            for (int pj = 0; pj < nColumnas; pj++){
+                printf("[%d]\t", matriz_d_resultado[pi][pj]);
+                matriz_d_datos[pi][pj] = matriz_d_resultado[pi][pj];
+            }
+            printf("\n");
+        }
+        pthread_barrier_wait(&mybarrier2);
     }
     
     
 
+    for (int i = 0; i < nHilos; i++){
+        pthread_join(vector_d_hilos[i], NULL);
+    }
+    
+   
 
+    pthread_barrier_destroy(&mybarrier1);
+    pthread_barrier_destroy(&mybarrier2);
     //LIBERAR MEMORIA DE vector_d_hilos
     free(vector_d_hilos);
     vector_d_hilos = NULL;
